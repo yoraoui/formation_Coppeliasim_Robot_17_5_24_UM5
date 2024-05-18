dbc32d6fef94e38cca196204cee9a001de87ff74
RAOUI Younès <51831506+yoraoui@users.noreply.github.com>
2024-05-18 17:19:35 +0100

    Update simulation_capteurs.py

--- simulation_capteurs.py
+++ simulation_capteurs.py
@@ -25,3 +25,35 @@ def sysCall_thread():
          sim.step() # resume in next simulation step
     
 # See the user manual or the available code snippets for additional callback functions and details
+
+
+"""
+#python
+import numpy as np
+
+def sysCall_init():
+    global camera_handle
+    sim = require('sim')
+
+    camera_handle = sim.getObjectHandle('Vision_sensor')
+    object_handle = sim.getObjectHandle('Sphere')
+    obj_position = sim.getObjectPosition(object_handle, -1)
+    cam_position = sim.getObjectPosition(camera_handle, -1)
+    cam_orientation = sim.getObjectOrientation(camera_handle, -1)
+    print("cam_position", cam_position)
+def sysCall_thread():
+    # Put your main code here, e.g.:
+    #
+    poses = np.array([[0,0,0],[0.45, 0.32,0], [1.01, -0.075,0], [0.37, -0.6, 0], [0.37, -0.6, 0]])    
+    i = 0
+    while not sim.getSimulationStopping():
+        p = poses[i,:]
+        euler = sim.getObjectOrientation(camera_handle, -1)
+
+        sim.setObjectPosition(camera_handle, -1, list(p))
+
+        sim.step() # resume in next simulation step
+        i += 1
+        if i == 4:
+            break
+"""
