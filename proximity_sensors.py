83e12f712ae99cc9797db61afe707d13f5fd80b5
RAOUI Younès <51831506+yoraoui@users.noreply.github.com>
2024-05-20 00:03:02 +0100

    Update proximity_sensors.py

--- proximity_sensors.py
+++ proximity_sensors.py
@@ -1,56 +1,53 @@
 #python
 
 def sysCall_init():
-    global joint_right
-    global joint_left
+    global wheel_right
+    global wheel_left
     global robot_handle
     global graph_x
     global graph_y
-    global graphHandle
+    global graphHandle_xy
+    global graphHandle_d
     global proximity
     global dist
     global graph
     global graph_distance
     sim = require('sim')
-    graphHandle = sim.getObject('/Graph')
+    graphHandle_xy = sim.getObject('/Graph1')
+    graphHandle_d = sim.getObject('/Graph2')
     proximity = sim.getObject('/Proximity_sensor')
-    graph_distance = sim.addGraphStream(graphHandle, 'distance',' ', 0, [1,0,0], 0)
-    joint_right = sim.getObject('/rightMotor')
-    joint_left = sim.getObject('/leftMotor')
+    wheel_right = sim.getObject('/rightMotor')
+    wheel_left = sim.getObject('/leftMotor')
     robot_handle = sim.getObject('/PioneerP3DX')
-    sim.setObjectInt32Param(joint_right,sim.jointintparam_dynctrlmode,sim.jointdynctrl_velocity)
-    sim.setObjectInt32Param(joint_left,sim.jointintparam_dynctrlmode,sim.jointdynctrl_velocity)
-    
-    sim.setJointTargetVelocity(joint_left,2)
-    sim.setJointTargetVelocity(joint_right,2)
-    graph_x = sim.addGraphStream(graphHandle, 'x', 'm',1)
-    graph_y = sim.addGraphStream(graphHandle, 'y', 'm',1)
-    curveId = sim.addGraphCurve(graphHandle, 'x/y', 2, [graph_x,graph_y], [0,0],'m by m',0,[1, 0, 0],2)
+    graph_distance = sim.addGraphStream(graphHandle_d, 'distance',proximity, 0, [0.1, 0.8, 0.2])
+                                    
+    graph_x = sim.addGraphStream(graphHandle_xy, 'x', 'g',1, [1, 0.8, 0.2])
+    graph_y = sim.addGraphStream(graphHandle_xy, 'y', 'g',1)
 
 def sysCall_actuation():
     t = sim.getSimulationTime()
     if (t>1 and dist>0.7) or (t>1 and dist==0):
         
-        sim.setJointTargetVelocity(joint_left,2)
-        sim.setJointTargetVelocity(joint_right,2)
+        sim.setJointTargetVelocity(wheel_left,2)
+        sim.setJointTargetVelocity(wheel_right,2)
         position = sim.getObjectPosition(robot_handle, sim.handle_world)
         orientation = sim.getObjectOrientation(robot_handle , sim.handle_world)
-        sim.setGraphStreamValue(graphHandle,graph_x,position[0])
-        sim.setGraphStreamValue(graphHandle,graph_y,position[1])
+        sim.setGraphStreamValue(graphHandle_xy,graph_x,position[0])
+        sim.setGraphStreamValue(graphHandle_xy,graph_y,position[1])
         
         print("dist = ", dist)
     elif(t>1 and dist<0.7):
-        sim.setJointTargetVelocity(joint_left,7)
-        sim.setJointTargetVelocity(joint_right,-7)
+        sim.setJointTargetVelocity(wheel_left,7)
+        sim.setJointTargetVelocity(wheel_right,-7)
     else:
-        sim.setJointTargetVelocity(joint_left,3)
-        sim.setJointTargetVelocity(joint_right,3)
+        sim.setJointTargetVelocity(wheel_left,3)
+        sim.setJointTargetVelocity(wheel_right,3)
 
 def sysCall_sensing():
     global dist
     res, dist, point, obj, n = sim.readProximitySensor(proximity)
     print("dist = ", dist)
-    sim.setGraphStreamValue(graphHandle,graph_distance,dist)
+    sim.setGraphStreamValue(graphHandle_d,graph_distance,dist)
 
 
     
